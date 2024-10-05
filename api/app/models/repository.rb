30d4e9a8b5962e1bc667301eaf39d80893947dbc
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-04 23:09:55 -0400

    Add .find_by_url scope on Repository model

--- api/app/models/repository.rb
+++ api/app/models/repository.rb
@@ -1,2 +1,8 @@
 class Repository < ApplicationRecord
+  def self.find_by_url(url)
+    uri = Addressable::URI.heuristic_parse(url)
+    return nil if uri.domain.nil? || uri.path.nil?
+
+    find_by(domain: uri.domain, path: uri.path)
+  end
 end
--- api/test/models/repository_test.rb
+++ api/test/models/repository_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 class RepositoryTest < ActiveSupport::TestCase
+  test ".find_by_url returns nil when the url is not valid" do
+    assert_nil(Repository.find_by_url("garbage url"))
+  end
+
+  test ".find_by_url returns nil if no repositories matches the url" do
+    assert_nil(Repository.find_by_url("https://github.com/example/repository"))
+  end
+
+  test ".find_by_url returns a repository that matches the url" do
+    assert_equal(repositories(:rails), Repository.find_by_url("https://github.com/rails/rails"))
+  end
+
+  test ".find_by_url returns a repository even when the scheme is missing" do
+    assert_equal(repositories(:rails), Repository.find_by_url("github.com/rails/rails"))
+  end
 end
