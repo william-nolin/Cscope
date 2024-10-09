25e4619dc38240a6d6f66f7aca48cb16847a2d6a
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-08 21:50:16 -0400

    Improve RepositorySyncService performance
    Change how RepositorySyncService writes to the database. Instead of
    writing on each itteration, we instead write in batch.
    
    We're accumulating 2500 commits in memory, then we insert all of them in
    one `INSERT` call.

--- api/app/services/repository_sync_service.rb
+++ api/app/services/repository_sync_service.rb
@@ -1,13 +1,17 @@
 class RepositorySyncService
+  COMMIT_BATCH_SIZE = 2500
+
   def initialize(repository)
     @repository = repository
+    @commits = []
+    @file_changes = []
   end
 
   def index
     git_repo = GitRepository.new(@repository.remote_url)
     git_repo.clone
     git_repo.logs(format: "||%H||%aN||%cs||%as||") do |logs|
-      commit_id = nil
+      current_commit_hash = nil
 
       logs.each do |line|
         line.force_encoding("utf-8")
@@ -15,16 +19,41 @@ class RepositorySyncService
         next if line == ""
 
         if line[0] == "|"
-          result = Commit.insert(commit_data_from_line(line))
-          commit_id = result.first["id"]
+          persist_current_batch if @commits.size >= COMMIT_BATCH_SIZE
+
+          commit = commit_data_from_line(line)
+          current_commit_hash = commit[:commit_hash]
+          @commits << commit
         elsif Integer(line[0], exception: false)
-          CommitFileChange.insert(commit_file_change_data_from_line(line, commit_id))
+          change = commit_file_change_data_from_line(line, current_commit_hash)
+          @file_changes << change
         end
       end
+
+      persist_current_batch
     end
   end
 
   private
+
+  def persist_current_batch
+    Commit.insert_all(@commits)
+
+    commits_by_hash = Commit
+      .select(:id, :commit_hash)
+      .where(repository: @repository, commit_hash: @commits.map { _1[:commit_hash] })
+      .index_by(&:commit_hash)
+
+    @file_changes.each do |change|
+      commit_hash = change.delete(:commit_hash)
+      change[:commit_id] = commits_by_hash[commit_hash].id
+    end
+
+    CommitFileChange.insert_all(@file_changes)
+
+    @commits = []
+    @file_changes = []
+  end
 
   def commit_data_from_line(line)
     _, hash, author, committer_date, author_date = line.split("||")
@@ -37,10 +66,10 @@ class RepositorySyncService
     }
   end
 
-  def commit_file_change_data_from_line(line, commit_id)
+  def commit_file_change_data_from_line(line, commit_hash)
     additions, deletions, filepath = line.split("\t", 3)
     {
-      commit_id: commit_id,
+      commit_hash: commit_hash,
       filepath: filepath.strip,
       additions: additions,
       deletions: deletions
