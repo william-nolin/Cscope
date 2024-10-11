250a03562dfe55556a2e8dd9c2cd17d3448944d9
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-10 22:08:12 -0400

    Add basic repositories in db/seeds.rb

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+Repository.find_or_create_by!(name: "rails", domain: "github.com", path: "/rails/rails")
+Repository.find_or_create_by!(name: "discourse", domain: "github.com", path: "/discourse/discourse")
