77340cb38ca20beeff324745e3fa518f985ffb64
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-13 15:32:20 -0400

    Add uniqueness constraint on SourceFileChange
    A commit can only change a file in one way.
    
    For example, It does not make sense to say:
      - Commit "abcdef" changes file A by adding 2 lines
      - Commit "abcdef" changes file A by adding 5 lines
    Instead, we say:
      - Commit "abcdef" changes file A by adding 7 lines
    
    This prevents recording multiple source file changes for the same file
    on a given commit.

--- api/app/models/source_file_change.rb
+++ api/app/models/source_file_change.rb
@@ -1,4 +1,6 @@
 class SourceFileChange < ApplicationRecord
   belongs_to :commit
   belongs_to :source_file
+
+  validates :source_file, uniqueness: { scope: :commit_id }
 end
--- api/test/models/source_file_change_test.rb
+++ api/test/models/source_file_change_test.rb
@@ -1,4 +1,9 @@
 require "test_helper"
 
 class SourceFileChangeTest < ActiveSupport::TestCase
+  test "cannot have 2 changes for a file on the same commit" do
+    duplicated_change = source_file_changes(:rails_4ad93b_Gemfile).dup
+    assert_not(duplicated_change.valid?)
+    assert(duplicated_change.errors.of_kind?(:source_file, :taken))
+  end
 end
