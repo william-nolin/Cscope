b24fc519e2948746546aa5f0de9a0964eb54bb26
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-12 20:30:26 -0400

    Rename `commit_file_changes` table
    Rename the `commit_file_changes` table to `source_file_changes`.

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_13_000442) do
-  create_table "commit_file_changes", force: :cascade do |t|
-    t.integer "commit_id"
-    t.string "filepath"
-    t.integer "additions", default: 0
-    t.integer "deletions", default: 0
-    t.index ["commit_id"], name: "index_commit_file_changes_on_commit_id"
-  end
-
+ActiveRecord::Schema[8.0].define(version: 2024_10_13_002642) do
   create_table "commits", force: :cascade do |t|
     t.integer "repository_id"
     t.string "commit_hash"
@@ -35,6 +27,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_13_000442) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["domain", "path"], name: "index_repositories_on_domain_and_path", unique: true
+  end
+
+  create_table "source_file_changes", force: :cascade do |t|
+    t.integer "commit_id"
+    t.string "filepath"
+    t.integer "additions", default: 0
+    t.integer "deletions", default: 0
+    t.index ["commit_id"], name: "index_source_file_changes_on_commit_id"
   end
 
   create_table "source_files", force: :cascade do |t|
