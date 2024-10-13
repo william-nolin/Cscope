6a606f3321c1f477307c4033f65d18d44f1c7444
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-12 21:01:05 -0400

    Fix SourceFile associations

--- api/app/models/source_file.rb
+++ api/app/models/source_file.rb
@@ -1,5 +1,7 @@
 class SourceFile < ApplicationRecord
   belongs_to :repository
+  has_many :source_file_changes
+  has_many :commits, through: :source_file_changes
 
   validates :filepath, uniqueness: { scope: :repository_id }
 end
