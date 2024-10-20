1afae5f03ce2f113a17ee7d8900742295ee51aff
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-19 22:29:34 -0400

    Add date filtering on commits over time endpoint
    Add date filtering on the CommitsController#commits_over_time action.
    Requests can now include a `start_date` and `end_date` url parameter to
    filter the data points.

--- api/test/controllers/commits_controller_test.rb
+++ api/test/controllers/commits_controller_test.rb
@@ -33,4 +33,45 @@ class CommitsControllerTest < ActionDispatch::IntegrationTest
       response.parsed_body
     )
   end
+
+  test "#commits_over_time output is filtered by `start_date` parameter" do
+    get "/repositories/#{@repository.id}/commits/stats/commits_over_time?start_date=2024-10-14"
+
+    assert_response(:ok)
+    assert_equal(
+      [
+        {
+          "date" => "2024-10-15",
+          "commits_count" => 1,
+          "modified_files" => 1,
+          "modified_lines" => 1
+        }
+      ],
+      response.parsed_body
+    )
+  end
+
+  test "#commits_over_time output is filtered by `end_date` parameter" do
+    get "/repositories/#{@repository.id}/commits/stats/commits_over_time?end_date=2024-10-14"
+
+    assert_response(:ok)
+    assert_equal(
+      [
+        {
+          "date" => "2024-10-13",
+          "commits_count" => 1,
+          "modified_files" => 1,
+          "modified_lines" => 7
+        }
+      ],
+      response.parsed_body
+    )
+  end
+
+  test "#commits_over_time output is filtered by both `start_date` and `end_date` parameter" do
+    get "/repositories/#{@repository.id}/commits/stats/commits_over_time?start_date=2020-01-01&end_date=2021-01-01"
+
+    assert_response(:ok)
+    assert_equal([], response.parsed_body)
+  end
 end
