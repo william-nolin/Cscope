c2231d1937970c58a54079eb3aa2fcae89b85837
Andre Godlove <74972747+godDeveloppeur@users.noreply.github.com>
2024-11-04 23:17:24 -0500

    Delete old front-end

--- OldFrondEnd/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript and JSX
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['<rootDir>/setupTests.ts']
-};
